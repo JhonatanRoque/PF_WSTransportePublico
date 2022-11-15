c3c3450ff9f6511ba89ac93f0689e6a3258fb1a4
JhonatanRoque <franciscoabarca7392@gmail.com>
2022-11-14 19:47:36 -0600

    pruebaDe conexion en nuevo hosting

--- connection_db.php
+++ connection_db.php
@@ -2,14 +2,14 @@
 function conexion(){
 	$conn = null;
 	$host = 'localhost';
-	$db = 'id19645168_db_transportfast';
-	$user = 'id19645168_root2';
-	$pwd = '2325trw./Q@6';
+	$db = 'u701462728_transportFast';
+	$user = 'u701462728_root';
+	$pwd = 'Jona1.than9';
 	
 try{
 	$conn = new PDO('mysql:host='.$host.'; dbname='.$db,$user,$pwd,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));  //Me pase a esta conexión por problemas de acentos, letra ñ y otros.
 	
-    //echo "Conexion hecha...";
+    echo "Conexion hecha...";
 }catch(PDOException $e){
 	echo "<center><h2 style='color:green'>Error al tratar de conectar a la BD.";
 	echo " consulte al soporte Técnico</h2></center>";
@@ -18,7 +18,7 @@ try{
 	return $conn;
 }
 
- //conexion();
+ conexion();
  	//by Tec. Wilson
     //Modificado por: Tec. Francisco Abarca
     //Fecha modificación: 07/11/2022 --:-- pm
