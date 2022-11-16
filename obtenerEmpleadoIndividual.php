036990d92a06d7dcda79edd8994db6068b266cc0
JhonatanRoque <franciscoabarca7392@gmail.com>
2022-11-15 19:04:28 -0600

    acepart carcaterres especiales

--- obtenerEmpleadoIndividual.php
+++ obtenerEmpleadoIndividual.php
@@ -1,6 +1,6 @@
 <?php
 include("empleado.php.php");
-$correo = $_POST['correo'];
+$correo = htmlspecialchars($_POST['correo'], ENT_QUOTES);
 
 if($correo != ""){
     $resultado = Empleados::listarEmpleadoIndividual($correo);
